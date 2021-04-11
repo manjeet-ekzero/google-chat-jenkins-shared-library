ce5530c6ac02d89e6e337efb80b9f5ad9fb386b4
manjeetsinh <manjeet.alonja@ekzero.com>
2021-04-12 00:16:46 +0530

    added google-chat sendnotification method

---
+++ vars/google-chat.groovy
@@ -0,0 +1,96 @@
+def sendNotification(
+    String google_chat_url,
+    String job_name,
+    String branch_name,
+    String build_number,
+    String status,
+    String gitCommit,
+    String authorName,
+    String job_url,
+    String gitRepoUrl,
+    String msg
+    ) {
+
+    template = """
+    {
+        "cards": [
+            {
+                "header": {
+                    "title": "${job_name}",
+                    "subtitle": "${branch_name}",
+                },
+                "sections": [
+                    {
+                        "header": "Build Info",
+                        "widgets": [
+                            {
+                                "keyValue": {
+                                    "topLabel": "Build No.",
+                                    "content": "${build_number}",
+                                }
+                            },
+                            {
+                                "keyValue": {
+                                    "topLabel": "Status",
+                                    "content": "${status}"
+                                }
+                            }
+                        ]
+                    },
+                    {
+                        "header": "Git Info",
+                        "widgets": [
+                            {
+                                "keyValue": {
+                                    "topLabel": "<b>Git Commit</b>",
+                                    "content": "${gitCommit}",
+                                }
+                            },
+                            {
+                                "keyValue": {
+                                    "topLabel": "Git aurther",
+                                    "content": "${authorName}"
+                                }
+                            }
+                        ]
+                    },
+                    {
+                        "widgets": [
+                            {
+                                "buttons": [
+                                    {
+                                        "textButton": {
+                                            "text": "OPEN PIPELINE",
+                                            "onClick": {
+                                                "openLink": {
+                                                    "url": "${job_url}"
+                                                }
+                                            }
+                                        }
+                                    },
+                                    {
+                                        "textButton": {
+                                            "text": "OPEN REPO",
+                                            "onClick": {
+                                                "openLink": {
+                                                    "url": "${gitRepoUrl}"
+                                                }
+                                            }
+                                        }
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+    """
+
+    sh """
+        curl -H 'Content-Type: application/json' \
+        -X POST "${google_chat_url}" \
+        --data '${template}'
+    """
+}
